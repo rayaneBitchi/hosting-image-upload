09dd010ec0381082988434583da03b82795c61b7
Rayane Bitchi <rayanebitchi@icloud.com>
2023-12-02 02:40:30 -0500

    scafold okay

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,45 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+    def index
+        @articles = Article.all
+    end
+    
+    def show
+    end
+    
+    def new
+        @article = Article.new
+    end
+    
+    def create
+        @article = Article.new(article_params)
+        if @article.save
+        redirect_to @article
+        else
+        render :new, status => :unprocessable_entity
+        end
+    end
+    
+    def edit
+    end
+    
+    def update
+        @article.update(article_params)
+        redirect_to @article
+    end
+    
+    def destroy
+        @article.destroy
+        redirect_to root_path, status => :see_other
+    end
+
+    private
+
+    def article_params
+        params.require(:article).permit(:title, :body)
+    end
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
 end
